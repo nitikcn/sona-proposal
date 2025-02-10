0f4ee1408b75bcc3012d11754bc54c4badadb293
Nitish Kumar <143488043+nitikcn@users.noreply.github.com>
2025-02-11 01:03:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
-	if (userInput === secretCode) {
+	if (userInput.toUpperCase() === secretCode.toUpperCase()) {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
